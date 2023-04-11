301242aab2a5cdf7b529a0234179255dcdbadb4b
nightkuzan <aekkarit12@gmail.com>
2023-04-12 03:37:58 +0700

    add Nav to mainlayout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,10 +1,16 @@
 <template>
   <nav>MainLayout</nav>
+  <Navbar />
   <router-view />
 </template>
 
 <script>
+  import Navbar from '@/layouts/components/Navbar.vue'
+
   export default {
     name: 'MainLayout',
+    components: {
+      Navbar,
+    },
   }
 </script>
